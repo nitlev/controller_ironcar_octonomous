2ad65fb78a38084ba337716a233d21fd20f64442
Veltin Dupont <vdupont@octo.com>
2019-05-20 10:28:17 +0200

    add tests

--- tests/units/infrastructure/test_async_filesystem_capture.py
+++ tests/units/infrastructure/test_async_filesystem_capture.py
@@ -1,8 +1,9 @@
 from queue import Queue
+from unittest.mock import MagicMock
 
 import numpy as np
 
-from python.infrastructure.filesystem.capture import AsyncCapture
+from python.infrastructure.filesystem.capture import AsyncCapture, AsyncFileSystemCaptureContext
 
 
 def test_async_capture_should_add_job_to_queue_when_saving_image():
@@ -33,3 +34,29 @@ def test_async_capture_should_not_add_job_to_queue_when_capture_is_disabled():
 
     # Then
     assert queue.empty()
+
+
+def test_capture_manager_should_return_capture_object():
+    # Given
+    writer = MagicMock()
+    expected_capture = MagicMock()
+
+    # When
+    with AsyncFileSystemCaptureContext(expected_capture, writer) as actual_capture:
+        # Then
+        assert actual_capture == expected_capture
+
+
+def test_capture_manager_should_start_writer_and_close_context():
+    # Given
+    writer = MagicMock()
+    expected_capture = MagicMock()
+
+    # When
+    with AsyncFileSystemCaptureContext(expected_capture, writer):
+        # Then
+        writer.start.assert_called_once()
+        writer.shutdown.assert_not_called()
+
+    # And Then
+    writer.shutdown.assert_called_once()
